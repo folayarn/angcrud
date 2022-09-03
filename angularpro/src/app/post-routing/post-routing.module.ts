dfbde0ed9fe9980df1f0eb81a1fda537567a2c7a
folayan shola <folayanshola@gmail.com>
2022-09-03 17:55:57 +0100

    new commit

---
+++ angularpro/src/app/post-routing/post-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from '../post/index/index.component';
+import { ViewComponent } from '../post/view/view.component';
+import { CreateComponent } from '../post/create/create.component';
+import { EditComponent } from '../post/edit/edit.component';
+  
+const routes: Routes = [
+  { path: '', redirectTo: 'post/index', pathMatch: 'full'},
+  { path: 'post/index', component: IndexComponent },
+  { path: 'post/:postId/view', component: ViewComponent },
+  { path: 'post/create', component: CreateComponent },
+  { path: 'post/:postId/edit', component: EditComponent } 
+];
+  
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PostRoutingModule { }
---
+++ angularpro/src/app/post/post.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
+import { ViewComponent } from './view/view.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostRoutingModule } from '../post-routing/post-routing.module';
+  
+@NgModule({
+  declarations: [IndexComponent, ViewComponent, CreateComponent, EditComponent],
+  imports: [
+    CommonModule,
+    PostRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class PostModule { }
